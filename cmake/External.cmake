b5b5e0a4c70c77e41fd018cda04df8489279a77f
Rashad Kanavath <mohammedrashadkm@gmail.com>
2015-12-08 02:21:01 +0100

    fixup macro

--- cmake/External.cmake
+++ cmake/External.cmake
@@ -9,12 +9,14 @@ function(external_project name)
   else()
     set(PKG_REPO ${PKG_NAME_})
   endif ()
-  
-  if(EXISTS ${ep_base}/Build/${PKG_NAME}/${PKG_NAME}Config.cmake)    
-    find_package(${PKG_NAME} PATHS ${ep_base}/Build/${PKG_NAME})
-  else()
+
+#      message(FATAL_ERROR "${ep_base}/Build/${PKG_NAME}/${PKG_NAME}Config.cmake")
+  # if(EXISTS ${ep_base}/Build/${PKG_NAME}/${PKG_NAME}Config.cmake)
+  # #   message(FATAL_ERROR "hmm. find_package")
+  #   find_package(${PKG_NAME} PATHS ${ep_base}/Build/${PKG_NAME})
+  # else()
     find_package(${PKG_NAME} )
-  endif()
+#  endif()
 
   if(NOT ${PKG_NAME}_FOUND)    
     ExternalProject_Add(${PKG_NAME}
@@ -25,27 +27,27 @@ function(external_project name)
       INSTALL_COMMAND
       )
     
-    set(${PKG_NAME}_INSTALL_DIR ${CMAKE_BINARY_DIR}/third-party/Install/${PKG_NAME})
+    set(${PKG_NAME}_INSTALL_DIR ${ep_base}/Install/${PKG_NAME})
     
-    if(NOT EXISTS "${CMAKE_BINARY_DIR}/third-party/Stamp/${PKG_NAME}/${PKG_NAME}-download")
+    if(NOT EXISTS "${ep_base}/Stamp/${PKG_NAME}/${PKG_NAME}-download")
       execute_process(COMMAND git clone ${EP_URL}/${PKG_REPO} ${PKG_NAME}
-        WORKING_DIRECTORY  ${CMAKE_BINARY_DIR}/third-party/Source)
+        WORKING_DIRECTORY  ${ep_base}/Source)
       
       execute_process(COMMAND ${CMAKE_COMMAND}  -E touch "${PKG_NAME}-download"
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/third-party/Stamp/${PKG_NAME} )  
+        WORKING_DIRECTORY ${ep_base}/Stamp/${PKG_NAME} )  
     endif()
     
-    execute_process(COMMAND ${CMAKE_COMMAND} ${CMAKE_BINARY_DIR}/third-party/Source/${PKG_NAME}
+    execute_process(COMMAND ${CMAKE_COMMAND} ${ep_base}/Source/${PKG_NAME}
       "-DCMAKE_INSTALL_PREFIX=${${PKG_NAME}_INSTALL_DIR}"
-      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/third-party/Build/${PKG_NAME} )
+      WORKING_DIRECTORY ${ep_base}/Build/${PKG_NAME} )
     
     #TODO: update lib_z repo
-    include(${ep_base}/Build/${PKG_NAME}/${PKG_NAME_}Targets.cmake)
+    include(${ep_base}/Build/${PKG_NAME}/${PKG_NAME}Config.cmake)
     
-    get_target_property(${PKG_NAME}_LIBRARIES_ ${PKG_NAME_} IMPORTED_LOCATION_NOCONFIG)
+    get_target_property(${PKG_NAME}_LIBRARY ${PKG_NAME_} IMPORTED_LOCATION_NOCONFIG)
     
     set(${PKG_NAME}_INCLUDE_DIRS ${${PKG_NAME}_INSTALL_DIR}/include PARENT_SCOPE)
-    set(${PKG_NAME}_LIBRARIES ${${PKG_NAME}_LIBRARIES_} PARENT_SCOPE)
+    set(${PKG_NAME}_LIBRARIES ${${PKG_NAME}_LIBRARY} PARENT_SCOPE)
     
   else()
     set(${PKG_NAME}_INCLUDE_DIRS ${${PKG_NAME}_INCLUDE_DIR} PARENT_SCOPE)
