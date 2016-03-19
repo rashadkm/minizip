99800f639ea405627aba2f9b1e9b7d681636cf3c
Rashad Kanavath <mohammedrashadkm@gmail.com>
2016-03-19 09:56:52 +0100

    missing uppercase'd cmake variable

--- cmake/SuperFind.cmake
+++ cmake/SuperFind.cmake
@@ -10,6 +10,7 @@ set(EP_URL "https://github.com/rashadkm")
 
 function(super_find_package name)
   set(PKG_NAME ${name})
+  string(TOUPPER ${name} PKG_NAME_)
   set(PKG_REPO ${PKG_NAME})
   set (extra_args ${ARGN})
   list(LENGTH extra_args num_extra_args)
